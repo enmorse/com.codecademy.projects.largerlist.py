419799becd7ba0a3da47f2fa228692d9118c98b7
enmorse <enmorse48@gmail.com>
2022-02-19 13:09:45 -0800

    Initial commit

---
+++ main.py
@@ -0,0 +1,8 @@
+# Write your function here
+def larger_list(lst1, lst2):
+    return [lst1[3] if lst1 == lst2 else lst2]
+
+
+# Uncomment the line below when your function is done
+print(larger_list([4, 10, 2, 5], [4, 10, 2, 5]))
+# print(larger_list([4, 10, 2, 5], [-10, 2, 5, 10]))
